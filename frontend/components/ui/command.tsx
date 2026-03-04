38d32f36519f15d3ed039f0488c1cec21a7ef969
mateonoel2 <mateonoel2@gmail.com>
2026-03-03 21:48:35 -0500

    Refactor ESLint configuration: replace .eslintrc.json with a simpler JSON file and remove outdated eslint.config.mjs. Update Next.js config to remove ESLint ignore settings. Change InputProps and SelectProps to use type alias instead of interface.

--- frontend/components/ui/command.tsx
+++ frontend/components/ui/command.tsx
@@ -1,12 +1,10 @@
 "use client";
 
 import * as React from "react";
-import { type DialogProps } from "@radix-ui/react-dialog";
 import { MagnifyingGlassIcon } from "@radix-ui/react-icons";
 import { Command as CommandPrimitive } from "cmdk";
 
 import { cn } from "@/lib/utils";
-import { Dialog, DialogContent } from "@/components/ui/dialog";
 
 const Command = React.forwardRef<
   React.ElementRef<typeof CommandPrimitive>,
